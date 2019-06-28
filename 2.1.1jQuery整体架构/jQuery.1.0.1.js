a5429234824f3bec7f36f499f32d1e607d2590f0
power <1410837981@qq.com>
2019-06-28 23:18:32 +0800

    2.1.1-extend方法的实现

--- 2.1.1jQuery整体架构/jQuery.1.0.1.js
+++ 2.1.1jQuery整体架构/jQuery.1.0.1.js
@@ -2,7 +2,7 @@
 	var jQuery = function() {
 		return new jQuery.prototype.init();
 	}
-	jQuery.prototype = {
+	jQuery.fn =jQuery.prototype = {
 		init: function() {
 
 		},
@@ -10,6 +10,66 @@
 
 		}
 	}
-	jQuery.prototype.init.prototype = jQuery.prototype;
+	//extend方法
+	jQuery.extend = jQuery.fn.extend = function() {
+		var target = arguments[0] || {};// 扩展的目标
+		var length = arguments.length;
+		var i = 1;
+		var deep = false; //是否深拷贝
+		var options, name, copy, clone, src,copyIsArray;
+		// 深拷贝
+		if (typeof target === 'boolean') {
+			deep = target;
+			target = arguments[1]
+		}
+		// 浅拷贝第一个参数是否为对象
+		if (typeof target !== 'object') {
+			target = {};
+		}
+		// 参数为1个时扩展到jQuery上
+		if (length === i) {
+			target = this;
+			i--;//拿到第一个扩展对象
+		}
+		// 浅拷贝
+		for(;i < length;i++) {
+			if((options = arguments[i]) !== null) {
+				for(name in options) {
+					src = target[name]; // 目标
+					copy = options[name];
+					// 深拷贝 (要拷贝的是对象或者是数组)
+					if(deep && (jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)))) {
+						if (copyIsArray) {
+							clone = jQuery.isArray(src)? src: []; // 目标对象
+						} else{
+							clone = jQuery.isPlainObject(src)? src: {}; //目标对象
+						}
+						target[name] = jQuery.extend(deep, clone, copy);
+					//浅拷贝
+					} else if(clone !== 'undefined') {
+						target[name] = copy;
+
+					}
+					
+				}
+			}
+		}
+		return target;
+
+	}
+
+	jQuery.extend({
+		isPlainObject: function(obj) {
+			return toString.call(obj) === '[object Object]';
+		},
+		isArray: function(obj) {
+			return toString.call(obj) === '[object Array]';
+		}
+	})
+
+
+
+
+	jQuery.fn.init.prototype = jQuery.prototype;
 	root.$ = root.jQuery = jQuery;
 })(this)
