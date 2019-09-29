be211a57f0b43bd563bc275eca61ef4a849e952b
power <1410837981@qq.com>
2019-09-29 14:54:45 +0800

    4.2、webpack核心概念

---
+++ 专题四-webpack/4.2-webpack核心概念/app.js
@@ -0,0 +1,5 @@
+import num from './module1.js'
+// import'babel-polyfill'
+setTimeout(() => {
+	 console.log(num)
+})
---
+++ 专题四-webpack/4.2-webpack核心概念/webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+	 mode: 'development',
+	 entry: {
+	 	 app: './app.js'
+	 },
+	 output: {
+	 	 path: __dirname + '/dist/js',
+	 	 filename: '[name].[hash:6].js'
+	 },
+	 // module: {
+	 // 	 rules: [{
+	 // 	 	 test: /\.js$/,
+		// 	  use: {
+	 // 	 	 	 loader: 'babel-loader',
+		// 		 options: {
+	 // 	 	 	 	 presets: [
+	 // 	 	 	 	 	['@babel/preset-env', {
+	 // 	 	 	 	 	 targets: {
+	 // 	 	 	 	 	 	 browsers: ['>1%'] // 以浏览器为目标
+		// 				 }
+		// 				}]
+		// 			 ]
+		// 			}
+		// 	  }
+		//  }]
+	 // }
+
+}
