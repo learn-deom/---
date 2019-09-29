3f31a7c04439e37b12569f0b0684051dd6cd5c1f
power <1410837981@qq.com>
2019-09-29 15:13:25 +0800

    js的编译-babel

--- 专题四-webpack/4.2-webpack核心概念/webpack.config.js
+++ 专题四-webpack/4.2-webpack核心概念/webpack.config.js
@@ -7,22 +7,22 @@ module.exports = {
 	 	 path: __dirname + '/dist/js',
 	 	 filename: '[name].[hash:6].js'
 	 },
-	 // module: {
-	 // 	 rules: [{
-	 // 	 	 test: /\.js$/,
-		// 	  use: {
-	 // 	 	 	 loader: 'babel-loader',
-		// 		 options: {
-	 // 	 	 	 	 presets: [
-	 // 	 	 	 	 	['@babel/preset-env', {
-	 // 	 	 	 	 	 targets: {
-	 // 	 	 	 	 	 	 browsers: ['>1%'] // 以浏览器为目标
-		// 				 }
-		// 				}]
-		// 			 ]
-		// 			}
-		// 	  }
-		//  }]
-	 // }
+	 module: {
+	 	 rules: [{
+	 	 	 test: /\.js$/,
+			  use: {
+	 	 	 	 loader: 'babel-loader',
+				 options: {
+	 	 	 	 	 presets: [
+	 	 	 	 	 	['@babel/preset-env', {
+	 	 	 	 	 	 targets: {
+	 	 	 	 	 	 	 browsers: ['>1%'] // 以浏览器为目标
+						 }
+						}]
+					 ]
+					}
+			  }
+		 }]
+	 }
 
 }
