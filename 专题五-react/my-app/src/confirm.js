cc1e7881820df46ad8c525b56aa3ad8f5240b785
power <1410837981@qq.com>
2019-10-26 20:57:50 +0800

    'react'

---
+++ 专题五-react/my-app/src/confirm.js
@@ -0,0 +1,12 @@
+import React, {Component,useEffect} from 'react'
+const confirm = async() => {
+	 try{
+		  useEffect(() => {
+				return true
+		  },[])
+	 }catch (e) {
+		  return false
+	 }
+}
+
+export default Confirm
