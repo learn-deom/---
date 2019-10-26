73b04cb186c17f1beb5e9d28728833c6156eed00
power <1410837981@qq.com>
2019-10-26 11:35:34 +0800

    专题五-受控和非受控组件

--- 专题五-react/my-app/src/App.js
+++ 专题五-react/my-app/src/App.js
@@ -1,31 +1,54 @@
 import React, {Component,useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 class InputNumber extends Component{
     constructor(props) {
         super(props)
-        this.state = {
-            value: ''
-        }
+		  this.state = {
+        	 innerValue: ''
+		  }
     }
-	 setValue(e) {
-        this.setState({
-            value: e.target.value
-        })
-    }
-    render() {
-        <input type='text' value={this.state.value}></input>
-    }
+    get isControl() {
+    	 return 'value' in this.props
+	 }
+	 get value() {
+    	 if(this.isControl) {
+    	 	 return this.props.value
+		 }else{
+    	 	 return this.state.innerValue
+		 }
+	 }
+	 render() {
+		  return (
+			  <input
+				  value={this.value}
+				  onChange={(e)=> {
+				  	 if(!this.isControl) {
+				  	 	 this.setState({
+							  innerValue: e.target.value
+						 })
+					 }
+					 this.props.onChange(e)
+				  }}
+			  />
+		  )
+	 }
+	 componentDidMount() {
+    	 console.log(this.props.innerValue)
+    	 this.setState({
+			  innerValue: this.props.defaultValue
+		 })
 
+	 }
 }
 function App(){
 	 const [value,setValue] = useState('aaa')
 	 return (
 		 <div>
-			  <InputNumber value={value} onChange={e=>{}}/>
+			  <InputNumber value={value} onChange={e=>setValue(
+				  e.target.value
+			  )}/>
 			  <InputNumber defaultValue={value} onChange={e=>{}}/>
 		 </div>
 	 )
 }
-
 export default App;
